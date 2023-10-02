ffb00a6e52da42a3c88f627b9bee3df5bf2b73ad
Adam Kelly <adam@cthulahoops.org>
2023-10-02 16:00:20 +0100

    Implement ping utility.

--- tiaw/__main__.py
+++ tiaw/__main__.py
@@ -1,8 +1,11 @@
 import struct
 from fcntl import ioctl
 from select import select
-from .icmp import make_ping, add_ip_header
+from . import icmp
 from . import ip
+import time
+from contextlib import contextmanager
+from dataclasses import dataclass
 
 
 def open_tun(tun_name):
@@ -24,10 +27,41 @@ def read_with_timeout(tun, n_bytes, timeout=1.0):
     return tun.read(n_bytes)
 
 
+@dataclass
+class TimingInfo:
+    start_time: float
+    end_time: float
+
+    def elapsed_time(self):
+        return self.end_time - self.start_time
+
+
+@contextmanager
+def timer():
+    info = TimingInfo(start_time=time.time(), end_time=None)
+
+    try:
+        yield info
+    finally:
+        info.end_time = time.time()
+
+
+def ping(destination):
+    for i in range(5):
+        # create ping packet
+        packet = icmp.add_ip_header(icmp.make_ping(seq=i))
+        # time how long it takes to get a reply
+        with timer() as timing_info:
+            tun.write(packet)
+            reply = tun.read(1024)
+
+        elapsed = round(timing_info.elapsed_time * 1000, 3)
+        ipv4 = ip.ipv4_from_bytes(reply[:20])
+        response = icmp.icmp_from_bytes(reply[20:])
+        print(
+            f"response from {destination}: icmp_seq={response.seq} ttl={ipv4.ttl} time={elapsed} ms"
+        )
+
+
 tun = open_tun("tun0")
-packet = add_ip_header(make_ping())
-tun.write(packet)
-reply = tun.read(1024)
-print(repr(reply))
-ipv4 = ip.ipv4_from_bytes(reply[:20])
-print(ipv4)
+ping("192.0.2.2")
